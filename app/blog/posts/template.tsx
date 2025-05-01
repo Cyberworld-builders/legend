2b284e588da6b8f24bfea69186551f69dac84aba
Jay <jaylong255@gmail.com>
2025-05-01 20:51:46 +0000

    updating the template

--- app/blog/posts/template.tsx
+++ app/blog/posts/template.tsx
@@ -1,18 +1,11 @@
-import Article from '@/components/Article';
-
 const markdownContent = `
-# Article Title
 
-This is a placeholder for your article content. Replace this with your Markdown content.
-
-## Section 1
-- List item 1
-- List item 2
-
-## Section 2
-Write your article here...
 `;
 
 export default function BlogPost() {
-  return <Article content={markdownContent} />;
+  return {
+    props: {
+      content: markdownContent
+    }
+  };
 }
