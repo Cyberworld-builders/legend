1945f40ac21b407627a5e65e44689aa21b188773
Jay Long <jay@cyberworldbuilders.com>
2025-05-01 15:55:35 -0500

    Create phone-test.tsx

---
+++ app/blog/posts/phone-test.tsx
@@ -0,0 +1,2 @@
+# phone test
+I created this entire commit and deployed it from my phone. I wrote the article and I used to get hug to commit merge and deploy it. I'm actually typing this with voice to text right now. 
